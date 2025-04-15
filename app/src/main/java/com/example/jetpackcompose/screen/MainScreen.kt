642e66071a1ab198d8921bc827e830d6c16c0ed9
Karim <tonygrandson420@gmail.com>
2025-04-15 13:40:41 +0600

    Make dialog

--- app/src/main/java/com/example/jetpackcompose/screen/MainScreen.kt
+++ app/src/main/java/com/example/jetpackcompose/screen/MainScreen.kt
@@ -1,6 +1,5 @@
 package com.example.jetpackcompose.screen
 
-import android.view.View.OnClickListener
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -9,14 +8,23 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -60,6 +68,7 @@ fun MainProfile(padding: PaddingValues) {
         }
     }
 }
+
 @Composable
 fun MainProfileImage(modifier: Modifier = Modifier) {
     Image(
@@ -97,11 +106,13 @@ fun MainProfileStats() {
         }
     }
 }
+
 @Composable
 fun MainProfileInfo(name: String, surname: String, description: String) {
     Name(name = name, surname = surname)
     Description(description = description)
 }
+
 @Composable
 fun Name(name: String, surname: String) {
     Row(
@@ -125,23 +136,87 @@ fun Name(name: String, surname: String) {
         )
     }
 }
+
 @Composable
 fun Description(description: String) {
+    var showDialog by remember { mutableStateOf(false) }
+
+    if (showDialog) {
+        EditableTextFieldDialog(onDismiss = { showDialog = false })
+    }
+
     Text(
-        modifier = Modifier.clickable { OnClickListener {
-            
-        } },
+        modifier = Modifier.clickable {
+            showDialog = true
+        },
         text = description,
         fontSize = 14.sp,
         fontWeight = FontWeight.W200
     )
-
 }
+
+
 
 @Composable
 @Preview
 fun MainProfilePreview() {
     MainProfile(padding = PaddingValues())
+}
+@Composable
+fun EditableTextFieldDialog(onDismiss: () -> Unit) {
+    var text by remember { mutableStateOf("Нажми, чтобы изменить") }
+    var showDialog by remember { mutableStateOf(false) }
+    var tempText by remember { mutableStateOf(text) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = text,
+            modifier = Modifier
+                .clickable {
+                    tempText = text
+                    showDialog = true
+                }
+                .padding(16.dp),
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+        if (showDialog) {
+            AlertDialog(
+                onDismissRequest = { showDialog = false },
+                title = {
+                    Text(text = "Редактировать текст")
+                },
+                text = {
+                    TextField(
+                        value = tempText,
+                        onValueChange = { tempText = it },
+                        singleLine = true
+                    )
+                },
+                confirmButton = {
+                    Button(onClick = {
+                        text = tempText
+                        showDialog = false
+                    }) {
+                        Text("Сохранить")
+                    }
+                },
+                dismissButton = {
+                    OutlinedButton(onClick = {
+                        showDialog = false
+                    }) {
+                        Text("Отмена")
+                    }
+                }
+            )
+        }
+    }
 }
 
 
